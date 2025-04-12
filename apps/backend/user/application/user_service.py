6139ad488b54cecf114c4950fac3a92c10c8bb86
Hojoong Jang <hojoong310@gmail.com>
2025-04-12 19:12:06 +0900

    feat: user save

---
+++ apps/backend/user/application/user_service.py
@@ -0,0 +1,38 @@
+from ulid import ULID
+from datetime import datetime
+from fastapi import HTTPException
+
+from user.domain.user import User
+from user.domain.repository.user_repo import IUserRepository
+from user.infra.repository.user_repo import UserRepository
+
+
+class UserService:
+    def __init__(self):
+        self.user_repo: IUserRepository = UserRepository()
+        self.ulid = ULID()
+
+    def create_user(self, name: str, email: str, password: str):
+        _user = None
+
+        try:
+            _user = self.user_repo.find_by_email(email)
+        except HTTPException as e:
+            if e.status_code != 422:
+                raise e
+
+        if _user:
+            raise HTTPException(status_code=422)
+        
+        now = datetime.now()
+        user: User = User(
+            id=self.ulid.generate(),
+            name=name,
+            email=email,
+            password=password,
+            created_at=now,
+            updated_at=now,
+        )
+        self.user_repo.save(user)
+
+        return user
