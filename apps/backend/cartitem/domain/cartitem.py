eb6348bed28d700ca9679101211b4a04c5738bbb
Hojoong Jang <hojoong310@gmail.com>
2025-09-17 22:53:02 +0900

    fix: cartitem domain

--- apps/backend/cartitem/domain/cartitem.py
+++ apps/backend/cartitem/domain/cartitem.py
@@ -7,7 +7,16 @@ class CartItem:
     id: str
     user_id: str
     product_id: str
-    product_option_id: str | None
+    is_active: bool
+    option_type_1: str | None
+    option_1: str | None
+    is_option_1_active: bool | None
+    option_type_2: str | None
+    option_2: str | None
+    is_option_2_active: bool | None
+    option_type_3: str | None
+    option_3: str | None
+    is_option_3_active: bool | None
     quantity: int
     created_at: datetime
     updated_at: datetime
