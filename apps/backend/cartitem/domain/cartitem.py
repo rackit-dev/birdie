3030de326f52600011baf412199a7dec6ef0dffb
Hojoong Jang <hojoong310@gmail.com>
2025-09-17 23:28:10 +0900

    fix: cartitem value to ids

--- apps/backend/cartitem/domain/cartitem.py
+++ apps/backend/cartitem/domain/cartitem.py
@@ -8,14 +8,14 @@ class CartItem:
     user_id: str
     product_id: str
     is_active: bool
-    option_type_1: str | None
-    option_1: str | None
+    option_type_1_id: str | None
+    option_1_id: str | None
     is_option_1_active: bool | None
-    option_type_2: str | None
-    option_2: str | None
+    option_type_2_id: str | None
+    option_2_id: str | None
     is_option_2_active: bool | None
-    option_type_3: str | None
-    option_3: str | None
+    option_type_3_id: str | None
+    option_3_id: str | None
     is_option_3_active: bool | None
     quantity: int
     created_at: datetime
