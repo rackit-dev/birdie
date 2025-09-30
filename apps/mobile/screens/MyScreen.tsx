2f71e7cde8b500a700ac20b348e63bbe4bff5edf
KangJiUng <woong236406@gmail.com>
2025-09-30 22:52:49 +0900

    Feat/Add a alert when logging out

--- apps/mobile/screens/MyScreen.tsx
+++ apps/mobile/screens/MyScreen.tsx
@@ -269,7 +269,18 @@ export default function MyScreen() {
               justifyContent: "space-between",
             }}
           >
-            <TouchableOpacity onPress={handleLogout}>
+            <TouchableOpacity
+              onPress={() => {
+                Alert.alert("로그아웃", "정말 로그아웃 하시겠습니까?", [
+                  { text: "취소", style: "cancel" },
+                  {
+                    text: "로그아웃",
+                    style: "destructive",
+                    onPress: handleLogout,
+                  },
+                ]);
+              }}
+            >
               <Text style={styles.menuText}>로그아웃</Text>
             </TouchableOpacity>
           </View>
